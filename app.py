55790f9ba687837262b0826bcfb5067b90e703a5
Yanz0666 <682024054@student.uksw.edu>
2026-07-19 20:57:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def create_app():
     return app
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=Config.DEBUG, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
